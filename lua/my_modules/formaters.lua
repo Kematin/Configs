22b4a83fd022e56df26f0e223dae4223b00dd861
kematin <kematin@inbox.ru>
2023-06-14 10:21:40 +0500

    add vscode to dotfiles

--- lua/my_modules/formaters.lua
+++ lua/my_modules/formaters.lua
@@ -76,7 +76,7 @@ prettier.setup({
     html_whitespace_sensitivity = "css",
     -- jsx_bracket_same_line = false,
     jsx_single_quote = false,
-    print_width = 80,
+    print_width = 85,
     prose_wrap = "preserve",
     quote_props = "as-needed",
     semi = true,
@@ -89,3 +89,25 @@ prettier.setup({
   },
 })
 
+prettier.setup({
+  cli_options = {
+    arrow_parens = "always",
+    bracket_spacing = true,
+    bracket_same_line = false,
+    embedded_language_formatting = "auto",
+    end_of_line = "lf",
+    html_whitespace_sensitivity = "css",
+    -- jsx_bracket_same_line = false,
+    jsx_single_quote = false,
+    print_width = 85,
+    prose_wrap = "preserve",
+    quote_props = "as-needed",
+    semi = true,
+    single_attribute_per_line = false,
+    single_quote = false,
+    tab_width = 4,
+    trailing_comma = "es5",
+    use_tabs = true,
+    vue_indent_script_and_style = false,
+  },
+})
